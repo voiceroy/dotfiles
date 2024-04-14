a0bcd76f53757deffcfc162caa6ecfa2069c6c5b
Harishankar G <harishankargaddanakeri@gmail.com>
2024-04-14 21:27:42 +0530

    Update path and create a function to update rust analyzer

---
+++ fish/functions/rau.fish
@@ -0,0 +1,5 @@
+function rau
+	mkdir -p ~/.local/bin
+	curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
+	chmod +x ~/.local/bin/rust-analyzer
+end
